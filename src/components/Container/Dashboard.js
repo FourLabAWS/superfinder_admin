bfa32dcc53510095896909d7d67fdf816cd4b5cf
홍유석 <ys101201@4-lab.com>
2023-03-08 17:54:34 +0900

    3/8 더미파일

--- src/components/Container/Dashboard.js
+++ src/components/Container/Dashboard.js
@@ -1,25 +1,26 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Stats from '../Table/Stats';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Stats from "../Table/Stats";
 const headingTextStyle = {
-    fontWeight: 550,
-}
+  fontWeight: 550,
+};
 export default function Container() {
-    return (
-        <Box
-            component="main"
-            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 2 }}
-        >
-            <br />
-            <Toolbar />
-            <Typography variant="h6" noWrap component="div"
-                sx={headingTextStyle}>
-                통계 데이터
-            </Typography>
-            <Toolbar />
-            <Stats />
-        </Box>
-    );
+  return (
+    <Box
+      component="main"
+      sx={{ flexGrow: 1, bgcolor: "background.default", p: 2 }}
+    >
+      <br />
+      <Toolbar />
+
+      <Typography variant="h6" noWrap component="div" sx={headingTextStyle}>
+        통계 데이터
+      </Typography>
+
+      <Toolbar />
+      <Stats />
+    </Box>
+  );
 }
