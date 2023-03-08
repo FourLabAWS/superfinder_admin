87b8bdf856f3c66203838cd31f1c8ef7f4b48cff
홍유석 <ys101201@4-lab.com>
2023-03-08 17:54:47 +0900

    3/8 더미파일

---
+++ src/components/Container/Dashboard_test.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Stats from "../Table/Stats";
+const headingTextStyle = {
+  fontWeight: 550,
+};
+export default function Container() {
+  return (
+    <Box
+      component="main"
+      sx={{ flexGrow: 1, bgcolor: "background.default", p: 2 }}
+    >
+      {/* 여기에 넣으세요 */}
+    </Box>
+  );
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,38 +1,69 @@
 import Dashboard from "../pages/Dashboard";
 import DataTable from "../pages/DataTable";
 import Management from "../pages/Management";
-import Login from '../pages/Login';
+import Login from "../pages/Login";
 import axios from "axios";
 import PublicRoute from "../components/Routes/PublicRoute";
 import PrivateRoute from "../components/Routes/PrivateRoute";
+import Notice from "../pages/Notice";
 
-const BASE_URL = "https://o0a46p97p0.execute-api.ap-northeast-2.amazonaws.com/v1/"
+const BASE_URL =
+  "https://o0a46p97p0.execute-api.ap-northeast-2.amazonaws.com/v1/";
 const client = axios.create({
-    baseURL: BASE_URL
+  baseURL: BASE_URL,
 });
 
-
 const routes = [
-    {
-        id: 1,
-        path: '/',
-        component: <PrivateRoute> <Dashboard /> </PrivateRoute>
-    },
-    {
-        id: 2,
-        path: '/management/:id',
-        component: <PrivateRoute> <Management /> </PrivateRoute>
-    },
-    {
-        id: 3,
-        path: '/analysis',
-        component: <PrivateRoute> <DataTable /> </PrivateRoute>
-    },
-    {
-        id: 4,
-        path: '/login',
-        component: <PublicRoute> <Login /> </PublicRoute>
-    }
-]
+  {
+    id: 1,
+    path: "/",
+    component: (
+      <PrivateRoute>
+        {" "}
+        <Dashboard />{" "}
+      </PrivateRoute>
+    ),
+  },
+  {
+    id: 2,
+    path: "/management/:id",
+    component: (
+      <PrivateRoute>
+        {" "}
+        <Management />{" "}
+      </PrivateRoute>
+    ),
+  },
+  {
+    id: 3,
+    path: "/analysis",
+    component: (
+      <PrivateRoute>
+        {" "}
+        <DataTable />{" "}
+      </PrivateRoute>
+    ),
+  },
+  {
+    id: 4,
+    path: "/login",
+    component: (
+      <PublicRoute>
+        {" "}
+        <Login />{" "}
+      </PublicRoute>
+    ),
+  },
+  {
+    id: 5,
+    path: "/notice",
+    component: (
+      <PublicRoute>
+        {" "}
+        <Notice />{" "}
+      </PublicRoute>
+    ),
+  },
+];
 
 export { routes, client };
