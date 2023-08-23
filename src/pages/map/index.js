93507da6ec2ebefff15cb5081d22f52eb77e2ae6
frvlv6 <frvlv6@4-lab.com>
2023-08-23 14:16:59 +0900

    마커 이미지 추가

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,42 +1,78 @@
 import React, { useEffect, useRef } from "react";
 import { MarkerClusterer } from "@googlemaps/markerclusterer";
 import { client } from "../../routes/routes";
+import { Logger } from "aws-amplify";
 
 const { google } = window;
 const WorldMap = () => {
+  // 23-08-22 유재혁
+  // s3 내 이미지 좌표를 이용해, 구글 맵에 좌표 설정.
   const mapRef = useRef(null);
+  // 0. ref는 js 내 getElementId와 같은 역할을 합니다.
   const [locations, setLocations] = React.useState([]);
 
-  const initMap = (latlng) => {
+  const initMap = (loc) => {
+    //1. 지도를 초기화 하기 위한 함수입니다.
     const map = new google.maps.Map(mapRef.current, {
+      // ref에 map을 직접 담습니다.
       center: { lat: 37.51175556, lng: 127.1079306 },
       zoom: 6,
       scrollwheel: false,
       streetViewControl: false,
     });
+    //2. 지도를 생성합니다. 위는 구글 맵에서 제공하는 로직입니다.
     const infoWindow = new google.maps.InfoWindow({
       content: "",
-      disableAutoPan: true,
+      disableAutoPan: false,
     });
-    // Create an array of alphabetical characters used to label the markers.
     const labels = "";
-    // Add some markers to the map.
-    const markers = latlng.map((position, i) => {
+    //3.  하나하나의 마커 위치와 모양을 세팅합니다.
+    //위도 경도 API를 호출해서, 받아온 위도 경도 배열을 map하여, 지도에 모두 뿌려줍니다.
+    const markers = loc.map((el, i) => {
+      console.log(el);
+      const position = el.latlng;
       const label = labels[i % labels.length];
       const marker = new google.maps.Marker({
         position,
         label,
       });
-      // markers can only be keyboard focusable when they have click listeners
-      // open info window when marker is clicked
+      //4. 마커에 이벤트 제공.
+      // marker.addListener("click", () => {
+      //   const content = `<img src="https://superfind.s3.ap-northeast-2.amazonaws.com/${el.img}" width="200" height="200 style="display: none;">`;
+      //   console.log("5", content);
+      //   infoWindow.setContent(content);
+      //   infoWindow.open(map, marker);
+      // });
+      // 프리로딩
       marker.addListener("click", () => {
-        infoWindow.setContent(label);
-        infoWindow.open(map, marker);
+        const img = new Image();
+        img.src = `https://superfind.s3.ap-northeast-2.amazonaws.com/${el.img}`;
+        img.onload = () => {
+          const content = `<img src="https://superfind.s3.ap-northeast-2.amazonaws.com/${el.img}" width="250" height="250 style="display: none;">`;
+          console.log("5", content);
+          infoWindow.setContent(content);
+          infoWindow.open(map, marker);
+        };
       });
+      // 지연 로딩.
+      // marker.addListener("click", () => {
+      //   const imgElement = document.createElement("img");
+      //   imgElement.src = `https://superfind.s3.ap-northeast-2.amazonaws.com/${el.img}`;
+      //   imgElement.width = 200;
+      //   imgElement.height = 200;
+
+      //   const content = document.createElement("div");
+      //   content.appendChild(imgElement);
+
+      //   console.log("5", content.innerHTML);
+      //   infoWindow.setContent(content);
+      //   infoWindow.open(map, marker);
+      // });
+
       return marker;
     });
 
-    // Add a marker clusterer to manage the markers.
+    //5. 멀리서 봤을 때의 마커들을 개수로 변경해주는 UI 말합니다.
     new MarkerClusterer({
       markers,
       map,
@@ -45,48 +81,26 @@ const WorldMap = () => {
   };
 
   useEffect(() => {
-    let test;
-    let latLng;
+    //6. API 요청을 통해, 위도 경도 배열을 받아와 지도에 렌더링합니다.
+    //FilterTable.js 파일의 useEffect를 보면 이해되실겁니다.
+    let locDta;
+
     const fetchLatLng = async () => {
       try {
         await client.get("getLatlng").then((res) => {
           console.log(res);
-          latLng = res.data.map((el) => ({
-            lat: parseFloat(el.lat),
-            lng: parseFloat(el.lng),
+          locDta = res.data.map((el) => ({
+            latlng: { lat: parseFloat(el.lat), lng: parseFloat(el.lng) },
+            img: el.img,
           }));
+
           //   test = [
-          //     { lat: -36.817685, lng: 175.699196 },
-          //     { lat: 31.817685, lng: 120.699196 },
-          //     { lat: 37.51175556, lng: 121.1079306 },
-          //     { lat: 37.11175556, lng: 121.1079306 },
-          //     { lat: -36.817685, lng: 171.699196 },
-          //     { lat: 31.817685, lng: 123.699196 },
-          //     { lat: 37.51175556, lng: 121.1079306 },
-          //     { lat: 37.11175556, lng: 127.1079306 },
-          //     { lat: -36.817685, lng: 171.699196 },
-          //     { lat: 31.817685, lng: 124.699196 },
-          //     { lat: 37.51175556, lng: 127.1079306 },
-          //     { lat: 37.11175556, lng: 123.1079306 },
-          //     { lat: -36.817685, lng: 177.699196 },
-          //     { lat: 31.817685, lng: 120.399196 },
-          //     { lat: 37.51175556, lng: 122.1079306 },
-          //     { lat: 37.11175556, lng: 120.1079306 },
-          //     { lat: -31.817685, lng: 177.699196 },
-          //     { lat: 36.817685, lng: 120.399196 },
-          //     { lat: 33.51175556, lng: 122.1079306 },
-          //     { lat: 32.11175556, lng: 120.1079306 },
-          //     { lat: -38.817685, lng: 177.699196 },
-          //     { lat: 39.817685, lng: 120.399196 },
-          //     { lat: 38.51175556, lng: 122.1079306 },
-          //     { lat: 5.11175556, lng: 120.1079306 },
-          //     { lat: 3.11175556, lng: 120.1079306 },
-          //     { lat: 1.11175556, lng: 120.1079306 },
-          //     { lat: -1.11175556, lng: 120.1079306 },
+          //     { lat: -36.817685, lng: 175.699196 }
           //   ];
-          setLocations(latLng);
+          setLocations(locDta);
         });
-        initMap(latLng);
+        console.log(locDta);
+        initMap(locDta);
       } catch (error) {
         console.log("ERROR", error);
       }
